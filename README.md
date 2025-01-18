c8b0c7e5daa7aecfd100aa1c26e9f8243f938ca8
Simeon <111970903+simeonnv@users.noreply.github.com>
2025-01-18 14:40:56 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Omicron
  Chatroom written in rust
+
+
+
+
+compiling tailwind
+```npx tailwindcss -i ./styles/input.css -o ./styles/output.css --watch```
+compiling frontend
+```trunk serve```
+compiling backend
+```cargo build```
---
+++ src/frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,rs,css}",
+    "./styles/**/*.{html,rs,css}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
