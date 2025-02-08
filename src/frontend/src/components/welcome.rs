2617aee1000b7494eb83094f2e92a94888469eb6
simeonnv2 <111970903+simeonnv@users.noreply.github.com>
2025-02-08 14:40:40 +0200

    favicon

--- src/frontend/src/components/welcome.rs
+++ src/frontend/src/components/welcome.rs
@@ -8,7 +8,7 @@ pub fn welcome() -> Html {
         <div class="align-middle justify-center items-center h-full flex flex-col gap-4">
             <h1 class="text-purple-600 text-2xl">{ "Welcome to Omicron" }</h1>
             <h2 class="text-purple-600 text-2xl">{ "Feel free to browse any subicron you wish!" }</h2>
-            <img class="h-80 w-80 border-4 !border-purple-600 border-dashed rounded-xl" src="/images/tigurche.gif"/>
+            <img class="h-80 w-80 border-4 !border-purple-600 border-dashed rounded-xl" src="/images/favicon.webp"/>
         </div>
     }
 }
