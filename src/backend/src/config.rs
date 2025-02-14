7716ab6b425f0d1a3f0b4a0da14a912eb6943690
simeonnv2 <111970903+simeonnv@users.noreply.github.com>
2025-02-14 15:48:15 +0200

    d

--- src/backend/src/config.rs
+++ src/backend/src/config.rs
@@ -4,7 +4,7 @@ pub const PORT: u16 = 6000;
 pub const LISTENING_ON: &str = "0.0.0.0";
 
 pub const DB_PORT: u16 = 3306;
-pub const DB_ADDRESS: &str = "database";
+pub const DB_ADDRESS: &str = "omicron_database";
 pub const DB_NAME: &str = "my_database";
 pub const DB_USERNAME: &str = "root";
 pub const DB_PASSWORD: &str = "root";
